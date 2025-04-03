08bb1b7508da0a8633ddca6aea1c4732471ef30e
Seba-Palominos <palominossebastian0@gmail.com>
2025-04-03 20:45:23 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Slim\App;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+$app->addBodyParsingMiddleware();
+
+(require __DIR__. '/../src/routes/usuarioRutas')($app) ;
+
+$app->run();
