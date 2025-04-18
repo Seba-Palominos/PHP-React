b1424e34802e737dbaa527cddf8c4e1c8021701a
Seba-Palominos <palominossebastian0@gmail.com>
2025-04-18 11:44:37 -0300

    correccion de middleware Alfanumerico

--- src/middlewares/Alfanumerico.php
+++ src/middlewares/Alfanumerico.php
@@ -9,26 +9,25 @@ use Slim\Psr7\Response;
 class Alfanumerico implements MiddlewareInterface{
     public function respuesta(Response $response,Array $msj){
         $response->getBody()->write(json_encode($msj));
-        $response = $response-> withHeader('content-type','application-json');
+        $response = $response-> withHeader('content-type','application/json');
         return $response;
     }
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface{
         $datos = $request->getParsedBody();
         //Strlen nos ayuda a contar la cantidad de caracteres que tiene el string
         $cantidadChar = strlen($datos['contraseña']);
-        if($cantidadChar < 6 || $cantidadChar > 20){
-            $response = new Response();
-            if($cantidadChar < 6 ){
-                return $this->respuesta($response,['error:'=>'faltan caracteres']);
-            }else{
-                if ($cantidadChar > 20){
-                    return $this->respuesta($response,['error'=> 'se pasa de 20 caracteres']);
-                }
+        if($cantidadChar < 6 ){
+            return $this->respuesta(new Response(),['error:'=>'faltan caracteres']);
+        }else{
+            if ($cantidadChar > 20){
+                return $this->respuesta(new Response(),['error'=> 'se pasa de 20 caracteres']);
             }
-        }
-        if (!preg_match('/[A-Z]/',$datos['contraseña']) && preg_match('/[0-9]/',$datos['contraseña']) && preg_match('/\W/',$datos['contraseña'])){
-            $response = new Response();
-            return $this->respuesta($response,['error'=> 'falta mayuscula,numero o caracter especial']); 
+            }
+        if (!preg_match('/[A-Z]/',$datos['contraseña']) || 
+            !preg_match('/[0-9]/',$datos['contraseña']) || 
+            !preg_match('/\W/',$datos['contraseña'])){
+                
+            return $this->respuesta(new Response,['error'=> 'falta mayuscula,numero o caracter especial']); 
         }
         
         return $handler->handle($request);
